7cdd87e347750c3d1049069243345c645323fc47
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-15 10:17:07 -0700

    knex-file-fix

--- knexfile.js
+++ knexfile.js
@@ -1,21 +1,21 @@
 module.exports = {
+
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./data/meals.db3"
-    },
+    client: 'sqlite3',
     useNullAsDefault: true,
-    migrations: {
-      directory: "./data/migrations",
-      tableName: "meals"
-    },
-    seeds: {
-      directory: "./data/seeds"
+    connection: {
+      filename: './data/meals.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    }
-  }
+        conn.run('PRAGMA foreign_keys = ON', done);
+      },
+    },
+    migrations: {
+      directory: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
 };
