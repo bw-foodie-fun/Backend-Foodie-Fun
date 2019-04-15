29147d59427cc59bdf3ee2d24a75df614a2c15c9
Aman <amsingh714@gmail.com>
2019-04-15 14:15:11 -0400

    able to login a new user and receive back a token

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -35,4 +35,37 @@ router.post("/register", (req, res) => {
   }
 });
 
+router.post("/login", (req, res) => {
+  let { username, password } = req.body;
+
+  if (!username || !password) {
+    res.status(400).json({
+      error: "Please provide a username and password."
+    });
+  } else {
+    db("users")
+      .where({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          const token = tokenService.generateToken(user);
+          res
+            .status(200)
+            .json({ message: `${user.username} is logged in.`, token });
+        } else {
+          res
+            .status(401)
+            .json({
+              error: "Please provide the correct username and password."
+            });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "There was an error while logging in."
+        });
+      });
+  }
+});
+
 module.exports = router;
