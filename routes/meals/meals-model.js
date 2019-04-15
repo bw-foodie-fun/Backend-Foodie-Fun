80bbe6be9c4377019a7945301b1d4776117c5564
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-15 16:33:47 -0700

    POST-meals-with-no-token

--- routes/meals/meals-model.js
+++ routes/meals/meals-model.js
@@ -2,8 +2,21 @@ const db = require('../../data/dbConfig.js');
 
 module.exports = {
     getAllMeals,
+    insert,
+    addMeal
 }
 
 function getAllMeals() {
     return db('meals')
+}
+
+function insert(meal) {
+    return db('meals')
+      .insert(meal)
+  }
+
+  async function addMeal(meal) {
+    const [id] = await db('meals').insert(meal, 'id');
+
+    return findPhotoById(id);
 }
--- routes/meals/meals-router.js
+++ routes/meals/meals-router.js
@@ -7,6 +7,7 @@ const mealsModel = require('./meals-model.js')
 const router = express.Router();
 
 //MEALS CRUD
+//GET ALL
 router.get('/all', async (req, res) => {
     try {
         const meals = await mealsModel.getAllMeals();
@@ -15,6 +16,35 @@ router.get('/all', async (req, res) => {
         res.status(500).json(error);
     }
 });
+//AUTHORIZED ONLY
+//POST
+router.post("/", async (req, res) => {
+    try {
+      const meal = await mealsModel.insert(req.body);
+      res.status(201).json(meal);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding the meal"
+      });
+    }
+  });
+
+//   router.post('/all/:id', restricted, checkRole('expat'), async (req, res) => {
+//     try {
+//         const { location, img_url } = req.body;
+//         req.body.user_id = req.params.id;
+//         if (!req.body.user_id || !location || !img_url) {
+//             res.status(400).json({ message: 'Please provide the user_id, location, and img_url for this photo' });
+//         } else {
+//             const photo = req.body
+//             const newPhoto = await Photos.addPhoto(photo);
+//             res.status(201).json(newPhoto);
+//         }
+//     } catch (error) {
+//         res.status(500).json(error);
+//     }
+// });
 
   //Export Router
 module.exports = router;
