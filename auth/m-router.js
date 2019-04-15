4dc96c5c4187855980ccaf3cc5becdd74cd1f536
Aman <amsingh714@gmail.com>
2019-04-15 18:19:09 -0400

    added a testing router

---
+++ auth/m-router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const db = require("../data/dbConfig");
+
+router.get("/", (req, res) => {
+  db("meals")
+    .then(meals => {
+      res.status(200).json(meals);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The meals could not be retrieved." });
+    });
+});
+
+module.exports = router;
