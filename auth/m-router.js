61c9a7851eddeede2f6efc8c2b418d76ffd433d0
Aman <amsingh714@gmail.com>
2019-04-15 19:13:47 -0400

    added a new route

--- auth/m-router.js
+++ auth/m-router.js
@@ -15,6 +15,26 @@ router.get("/", restricted, (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
 
+  db("meals")
+    .where({ id })
+    .first()
+    .then(meal => {
+      if (meal) {
+        res.status(200).json(meal);
+      } else {
+        res
+          .status(404)
+          .json({ error: "The meal with the specified ID does not exist." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The action with the specified ID could not be retrieved"
+      });
+    });
+});
 
 module.exports = router;
