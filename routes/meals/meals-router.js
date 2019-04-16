e9605b229076ff553eae5e66da01c7eaba59b522
Aman <amsingh714@gmail.com>
2019-04-15 20:22:03 -0400

    commited laryna's routes

--- routes/meals/meals-router.js
+++ routes/meals/meals-router.js
@@ -16,5 +16,19 @@ router.get('/all', async (req, res) => {
     }
 });
 
+// AUTHORIZED ONLY
+// POST
+router.post("/", async (req, res) => {
+   try {
+     const meal = await mealsModel.insert(req.body);
+     res.status(201).json(meal);
+   } catch (error) {
+     console.log(error);
+     res.status(500).json({
+       message: "Error adding the meal"
+     });
+   }
+ });
+
   //Export Router
 module.exports = router;
