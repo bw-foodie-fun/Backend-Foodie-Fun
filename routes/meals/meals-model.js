89d8cda4236c70a077856f65c877e18077dca407
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-04-15 18:13:14 -0700

    post-restricted-with-token

--- routes/meals/meals-model.js
+++ routes/meals/meals-model.js
@@ -10,13 +10,14 @@ function getAllMeals() {
     return db('meals')
 }
 
-function insert(meal) {
-    return db('meals')
-      .insert(meal)
-  }
-
   async function addMeal(meal) {
     const [id] = await db('meals').insert(meal, 'id');
 
-    return findPhotoById(id);
+    return findMealById(id);
+}
+
+function findMealById(id) {
+    return db('meals')
+        .where({ id })
+        .first();
 }
