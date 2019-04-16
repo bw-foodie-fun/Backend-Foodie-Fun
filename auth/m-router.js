e6712be1e2e56c38a16dc5129e53bd75efceb331
Aman <amsingh714@gmail.com>
2019-04-16 09:59:53 -0400

    user_id know accessible to crud

--- auth/m-router.js
+++ auth/m-router.js
@@ -15,11 +15,22 @@ router.get("/", restricted, (req, res) => {
     });
 });
 
+router.get("/all", (req, res) => {
+  db("meals")
+    .then(meals => {
+      res.status(200).json(meals);
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The meals could not be retrieved." });
+    });
+});
+
+
 router.get("/:id", restricted, (req, res) => {
   const { id } = req.params;
 
   db("meals")
-    .where({ id })
+    .where({ id, user_id: req.decodedToken.subject })
     .first()
     .then(meal => {
       if (meal) {
@@ -27,7 +38,7 @@ router.get("/:id", restricted, (req, res) => {
       } else {
         res
           .status(404)
-          .json({ error: "The meal with the specified ID does not exist." });
+          .json({ error: "You cannot access the meal with this specific id." });
       }
     })
     .catch(error => {
@@ -37,14 +48,13 @@ router.get("/:id", restricted, (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
+router.post("/", restricted, (req, res) => {
   const meal = req.body;
 
   if (!meal.item_name) {
     res.status(400).json({ error: "Please provide a name for the meal." });
   } else {
-    // meal.user_id = req.decodedToken.subject;
-    meal.user_id = 3;
+    meal.user_id = req.decodedToken.subject;
     db("meals")
       .insert(meal)
       .then(ids => {
@@ -57,13 +67,62 @@ router.post("/", (req, res) => {
           });
       })
       .catch(error => {
-        res
-          .status(500)
-          .json({
-            error: "There was an error while saving the meal to the database."
-          });
+        res.status(500).json({
+          error: "There was an error while saving the meal to the database."
+        });
       });
   }
+});
+
+router.put("/:id", restricted, (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  if (!changes.item_name) {
+    res.status(400).json({
+      error: "Please provide a name for the meal."
+    });
+  } else {
+    db("meals")
+      .where({ id, user_id: req.decodedToken.subject })
+      .update(changes)
+      .then(count => {
+        if (count > 0) {
+          res.status(200).json(count);
+        } else {
+          res.status(404).json({
+            error: "You cannot access the meal with this specific id."
+          });
+        }
+      })
+      .catch(error => {
+        res.status(500).json({
+          error: "The meal could not be modified."
+        });
+      });
+  }
+});
+
+router.delete("/:id", restricted, (req, res) => {
+  const { id } = req.params;
+
+  db("meals")
+    .where({ id, user_id: req.decodedToken.subject })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json(count);
+      } else {
+        res
+          .status(404)
+          .json({ error: "You cannot access the meal with this specific id." });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: "The meal could not be removed."
+      });
+    });
 });
 
 module.exports = router;
