95490d97de802c3d6f20e2804ad441286e250800
Aman <amsingh714@gmail.com>
2019-04-17 12:05:59 -0400

    added production db config

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,15 +2,33 @@ const request = require("supertest");
 const server = require("./server.js");
 
 const db = require("../data/dbConfig");
-const restricted = require("../auth/restricted");
 
- 
 afterEach(async () => {
   await db("meals").truncate();
 });
 
+let token;
+let user = {
+  username: "sansa",
+  password: "password"
+}
+beforeAll((done) => {
+  request(server)
+    .post('/api/auth/login')
+    .send({
+      username: user.username,
+      password: user.password,
+    })
+    .end((err, res) => {
+      token = res.body.token; 
+      done();
+    });
+});
+
+
 describe("server.js", () => {
   describe("GET /api/meals", () => {
+
     it("should set testing enviroment", () => {
       expect(process.env.DB_ENV).toBe("testing");
     });
@@ -31,13 +49,14 @@ describe("server.js", () => {
         });
     });
 
-    // it("should return status code 200 when authorized.", restricted, () => {
+    // it("should return status code 200 when authorized.", done => {
     //   return request(server)
     //     .get("/api/meals")
-    //     .then(res => {
-    //       expect(res.status).toBe(401);
-    //     });
+    //     .set('Authorization', 'Bearer ' + token)
+    //     .expect(200, done)
+        // .then(res => {
+        //   expect(res.status, done).toBe(200);
+        // });
     // });
-
   });
 });
