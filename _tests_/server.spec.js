2b3c00c4646f63ade2d3c7814f9d8489c44d9278
Aman <amsingh714@gmail.com>
2019-04-18 19:14:23 -0400

    merged tests

--- _tests_/server.spec.js
+++ _tests_/server.spec.js
@@ -2,13 +2,9 @@ const request = require("supertest");
 const db = require("../data/dbConfig.js");
 const server = require("../api/server.js");
 
-afterEach(async () => {
-  await db("meals").truncate();
-});
-
-//GLOBAL
+//GLOBAL--------------------------------------------------------------
 let token;
-
+//POST LOGIN, RECEIVE TOKEN, token used for TESTSs
 beforeAll(async done => {
   await db("users").truncate();
   request(server)
@@ -18,7 +14,7 @@ beforeAll(async done => {
       password: "pw"
     })
     .end((err, response) => {
-      //RESPONSE LOG IF NEEDED
+      //RESPONSE LOG IF NEEDED to see variables/objects being stored locations
       //    console.log(response)
       token = response.body.token; // save the token!
       done();
@@ -27,12 +23,15 @@ beforeAll(async done => {
 
 //TEST SUITES
 describe("server.js", () => {
+  let meal = {
+    item_name: "HOT DOG"
+  };
   describe("GET /api/meals", () => {
-    it("should set testing enviroment", () => {
+    it("should set testing environment", () => {
       expect(process.env.DB_ENV).toBe("testing");
     });
 
-    it.only("should return JSON", () => {
+    it("should return JSON", () => {
       return request(server)
         .get("/api/meals")
         .then(res => {
@@ -48,14 +47,110 @@ describe("server.js", () => {
         });
     });
 
-    it("should return status code 200 when authorized", () => {
-      console.log("TOKEN", token);
+    it.skip("should return status code 200 when authorized", () => {
       return request(server)
-        .get("/api/meals")
         .set("Authorization", `${token}`)
+        .get("/api/meals")
         .then(response => {
           expect(response.statusCode).toBe(200);
         });
     });
   });
+
+  describe("POST /api/meals", () => {
+    let meal = {
+      item_name: "BURGER"
+    };
+    it("should return JSON", () => {
+      return request(server)
+        .post("/api/meals")
+        .set("Authorization", `${token}`)
+        .send(meal)
+        .then(res => {
+          expect(res.type).toBe("application/json");
+        });
+    });
+
+    it("should return status code 401 when not authorized.", () => {
+      return request(server)
+        .post("/api/meals")
+        .then(res => {
+          expect(res.status).toBe(401);
+        });
+    });
+
+    it("should return status code 200 when authorized", () => {
+      return request(server)
+        .post("/api/meals")
+        .set("Authorization", `${token}`)
+        .send(meal)
+        .then(response => {
+          expect(response.statusCode).toBe(201);
+        });
+    });
+  });
+
+  describe("PUT /api/meals", () => {
+    it("should return JSON", () => {
+      return request(server)
+        .put(`/api/meals/${meal.id}`)
+        .set("Authorization", `${token}`)
+        .send(meal)
+        .then(res => {
+          expect(res.type).toBe("application/json");
+        });
+    });
+
+    it("should return status code 401 when not authorized.", () => {
+      return request(server)
+        .put(`/api/meals/${meal.id}`)
+        .then(res => {
+          expect(res.status).toBe(401);
+        });
+    });
+
+    it.skip("should return status code 200 when authorized", () => {
+      return request(server)
+        .set("Authorization", `${token}`)
+        .put(`/api/meals/18`)
+        .send(meal)
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+        });
+    });
+  });
+
+  describe("DELETE /api/meals", () => {
+    it("should return JSON", () => {
+      return request(server)
+        .delete(`/api/meals/${meal.id}`)
+        .set("Authorization", `${token}`)
+        .send(meal)
+        .then(res => {
+          expect(res.type).toBe("application/json");
+        });
+    });
+
+    it("should return status code 401 when not authorized.", () => {
+      return request(server)
+        .delete(`/api/meals/${meal.id}`)
+        .then(res => {
+          expect(res.status).toBe(401);
+        });
+    });
+
+    it.skip("should return status code 200 when authorized", () => {
+      return request(server)
+        .set("Authorization", `${token}`)
+        .put(`/api/meals/${meal.id}`)
+        .send(meal)
+        .then(response => {
+          expect(response.statusCode).toBe(200);
+        });
+    });
+  });
+});
+
+afterEach(async () => {
+  await db("meals").truncate();
 });
